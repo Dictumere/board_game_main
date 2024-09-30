fd00407a7af37c2a094dfe26e23e52ac2f1a77b4
Dictumere <maksimsaltaev03@gmail.com>
2024-09-30 21:17:33 +0300

    Main file

---
+++ main.py
@@ -0,0 +1,81 @@
+import file_operations as fo
+from faker import Faker
+import random
+import os
+
+
+fake = Faker("ru_RU")
+skills = ["Стремительный прыжок", "Электрический выстрел", "Ледяной удар", "Стремительный удар", "Кислотный взгляд",
+          "Тайный побег", "Ледяной выстрел", "Огненный заряд", ]
+letters = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
+
+
+def create_character(number):
+    rand_skills = random.sample(skills, k=3)
+    runic_skill = []
+
+    for skill in rand_skills:
+        for key, value in letters.items():
+            skill = skill.replace(key, value)
+        runic_skill.append(skill)
+
+    name = {
+        "first_name": fake.first_name(),
+        "last_name": fake.last_name(),
+        "job": fake.job(),
+        "town": fake.city(),
+
+        "strength": random.randint(3, 18),
+        "agility": random.randint(3, 18),
+        "endurance": random.randint(3, 18),
+        "intelligence": random.randint(3, 18),
+        "luck": random.randint(3, 18),
+
+        "skill_1": runic_skill[0],
+        "skill_2": runic_skill[1],
+        "skill_3": runic_skill[2]
+    }
+    return name
+
+
+def save_character(number, character):
+    filename = f"result_{number}.svg"
+    full_path = os.path.join("images", filename)
+    fo.render_template("charsheet.svg", full_path, character)
+
+
+def main():
+    if not os.path.exists("images"):
+        os.makedirs("images")
+
+    for number in range(10):
+        character = create_character(number + 1)
+        save_character(number + 1, character)
+
+
+if __name__ == '__main__':
+    main()
